613eb3a31c6a74e34236fa7de3235b69fd811322
Pat Mac <itgpmc@gmail.com>
2016-12-10 19:29:03 -0800

    add react-color to UI

--- www/build.sh
+++ www/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+set -e
+
 npm install
 ./node_modules/.bin/webpack -d
--- www/src/lightsdude.jsx
+++ www/src/lightsdude.jsx
@@ -1,7 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Tab, MenuItem, Row, Col, Nav, NavItem, NavDropdown } from 'react-bootstrap';
+import { Button, Tab, MenuItem, Row, Col, Nav, NavItem, NavDropdown } from 'react-bootstrap';
+import { SketchPicker } from 'react-color';
 
+
+class SingleLightsPane extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            r: 155,
+            g: 155,
+            b: 155
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleColorChange = this.handleColorChange.bind(this);
+    }
+
+    getColorHex() {
+        var r = (this.state.r < 16 ? "0" : "") + this.state.r.toString(16),
+            g = (this.state.g < 16 ? "0" : "") + this.state.g.toString(16),
+            b = (this.state.b < 16 ? "0" : "") + this.state.b.toString(16);
+        return '#' + r + g + b;
+    }
+
+    handleSubmit(event) {
+        var curstate = this.state;
+        alert('clicked submit for single lights (color: '+this.getColorHex()+')');
+        event.preventDefault();
+    }
+
+    handleColorChange(color, event) {
+        this.setState({
+            r: color.rgb.r,
+            g: color.rgb.g,
+            b: color.rgb.b
+        });
+    }
+
+    render() {
+        const buttonStyle = {
+            display: 'inline-block',
+            marginLeft: 10,
+            verticalAlign: 'top'
+        }, pickerStyle = {
+            display: 'inline-block'
+        };
+
+        return (
+            <div>
+                <SketchPicker disableAlpha={true} color={this.getColorHex()}
+                              onChangeComplete={this.handleColorChange}
+                              style={pickerStyle} width={400} />
+                <Button type="button" bsSize="large"
+                        bsStyle="success" onClick={this.handleSubmit}
+                        style={buttonStyle}>Change</Button>
+            </div>
+        );
+    }
+}
 
 class App extends React.Component {
     render() {
@@ -26,7 +83,9 @@ class App extends React.Component {
                     </Col>
                     <Col sm={12}>
                         <Tab.Content>
-                            <Tab.Pane eventKey="single">todo (single)</Tab.Pane>
+                            <Tab.Pane eventKey="single">
+                                <SingleLightsPane />
+                            </Tab.Pane>
                             <Tab.Pane eventKey="blink">todo (blink)</Tab.Pane>
                             <Tab.Pane eventKey="christmas">todo (christmas)</Tab.Pane>
                         </Tab.Content>
